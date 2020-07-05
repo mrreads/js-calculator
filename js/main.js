87f929e9e468293a73babc9c1c03e9347c83192d
Alexander <mrreads@yandex.com>
2020-07-05 15:19:19 +0300

    refactoring

--- js/main.js
+++ js/main.js
@@ -7,33 +7,49 @@ let buttonActionIsPressed = 0;
 let resultNotFirst = 0;
 
 let numbers = document.querySelectorAll(".btnNumber");
+
+// Ввод c клавиатуры
+document.addEventListener("keydown", function (event) 
+{
+    // Цифры
+    for(let i = 0; i < numbers.length; i++)
+    {
+        if (event.code == `Digit${numbers[i].textContent}`)
+        { 
+            if (resultNotFirst == 0 || resultNotFirst == 1)
+            {
+                if (buttonActionIsPressed == 0)
+                    tempOne.textContent += numbers[i].textContent;
+                
+                if (buttonActionIsPressed == 1)
+                    tempThree.textContent += numbers[i].textContent;
+            }
+        }
+    }
+
+    // Стереть
+    if (event.code == 'Backspace')
+    {
+        if (buttonActionIsPressed == 0)
+            tempOne.textContent = tempOne.textContent.slice(0, -1);
+        
+        if (buttonActionIsPressed == 1)
+            tempThree.textContent = tempThree.textContent.slice(0, -1);
+    }
+});
+
 for(let i = 0; i < numbers.length; i++)
 {
     numbers[i].addEventListener("click", function()
     {
-        if (resultNotFirst == 0)
+        if (resultNotFirst == 0 || resultNotFirst == 1)
         {
             if (buttonActionIsPressed == 0)
-            {
                 tempOne.textContent += numbers[i].textContent;
-            }
+            
             if (buttonActionIsPressed == 1)
-            {
                 tempThree.textContent += numbers[i].textContent;
-            }
         }
-        if (resultNotFirst == 1)
-        {
-            if (buttonActionIsPressed == 0)
-            {
-                tempOne.textContent += numbers[i].textContent;
-            }
-            if (buttonActionIsPressed == 1)
-            {
-                tempThree.textContent += numbers[i].textContent;
-            }
-        }
-        
     });
 }
 
@@ -51,60 +67,44 @@ for(let i = 0; i < actions.length; i++)
     });
 }
 
+
+// кнопка очистки
 btnClean.addEventListener("click", function()
 {
     if (buttonActionIsPressed == 0)
-    {
-        tempOne.textContent = tempOne.textContent.slice(0, -1);
-    }
+        if (tempOne.textContent != '')
+            tempOne.textContent = tempOne.textContent.slice(0, -1);
+
     if (buttonActionIsPressed == 1)
-    {
-        tempThree.textContent = tempThree.textContent.slice(0, -1);
-    }
+        if (tempThree.textContent == '')
+        {   
+            buttonActionIsPressed = 0;
+            tempTwo.textContent = '';
+        }
+        else
+            tempThree.textContent = tempThree.textContent.slice(0, -1);
 });
 
+// подчёт результата
 btnEqual.addEventListener("click", function()
 {
     if (buttonActionIsPressed == 1 && tempOne.textContent != "" && tempThree.textContent != "")
     {
         if (actionType == "plus")
-        {
-            if ((parseFloat(tempOne.textContent) == 0.1 && parseFloat(tempThree.textContent) == 0.2) || (parseFloat(tempOne.textContent) == 0.2 && parseFloat(tempThree.textContent) == 0.1))
-            {
-                tempOne.textContent = +(parseFloat(tempOne.textContent) + parseFloat(tempThree.textContent)).toFixed(1);
-            }
-            else
-            {
-                tempOne.textContent = parseFloat(tempOne.textContent) + parseFloat(tempThree.textContent);
-            }
-            tempTwo.textContent = "";
-            tempThree.textContent = "";
-            resultNotFirst = 1;
-            buttonActionIsPressed = 0;
-        }
+            tempOne.textContent = +(+tempOne.textContent + +tempThree.textContent).toFixed(1);
+        
         if (actionType == "minus")
-        {
-            tempOne.textContent = parseFloat(tempOne.textContent) - parseFloat(tempThree.textContent);
-            tempTwo.textContent = "";
-            tempThree.textContent = "";
-            resultNotFirst = 1;
-            buttonActionIsPressed = 0;
-        }
+            tempOne.textContent = +(+tempOne.textContent - +tempThree.textContent).toFixed(1);
+        
         if (actionType == "multiply")
-        {
-            tempOne.textContent = parseFloat(tempOne.textContent) * parseFloat(tempThree.textContent);
-            tempTwo.textContent = "";
-            tempThree.textContent = "";
-            resultNotFirst = 1;
-            buttonActionIsPressed = 0;
-        }
+            tempOne.textContent = +(+tempOne.textContent * +tempThree.textContent).toFixed(1);
+        
         if (actionType == "divide")
-        {
-            tempOne.textContent = parseFloat(tempOne.textContent) / parseFloat(tempThree.textContent);
-            tempTwo.textContent = "";
-            tempThree.textContent = "";
-            resultNotFirst = 1;
-            buttonActionIsPressed = 0;
-        }
+            tempOne.textContent = +(+tempOne.textContent / +tempThree.textContent).toFixed(1);
+        
+        tempTwo.textContent = "";
+        tempThree.textContent = "";
+        resultNotFirst = 1;
+        buttonActionIsPressed = 0;
     }
 });
